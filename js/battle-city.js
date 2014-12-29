4588fcd3a64f351aaee8c029aa85a6717a137645
ChemikPIL <chemikpil@gmail.com>
2014-12-29 14:52:55 +0100

    Add bullets

--- js/battle-city.js
+++ js/battle-city.js
@@ -7,7 +7,7 @@
       w: screen.canvas.width, 
       h: screen.canvas.height
     };
-    this.players = [].concat(new Player(this));
+    this.bodies = [].concat(new Player(this));
     
     var self = this;
     var tick = function () {
@@ -21,9 +21,9 @@
   
   Game.prototype = {
     update: function () {
-      for (var i = 0, l = this.players.length; i < l; i++) {
-        if (this.players[i].update !== undefined) {
-          this.players[i].update(screen);
+      for (var i = 0, l = this.bodies.length; i < l; i++) {
+        if (this.bodies[i].update !== undefined) {
+          this.bodies[i].update(screen);
         }
       }
     },
@@ -31,10 +31,21 @@
     draw: function (screen) {
       screen.clearRect(0, 0, this.size.w, this.size.h);
       
-      for (var i = 0, l = this.players.length; i < l; i++) {
-        if (this.players[i].draw !== undefined) {
-          this.players[i].draw(screen);
+      for (var i = 0, l = this.bodies.length; i < l; i++) {
+        if (this.bodies[i].draw !== undefined) {
+          this.bodies[i].draw(screen);
         }
+      }
+    },
+    
+    addBody: function (body) {
+      this.bodies.push(body);
+    },
+    
+    removeBody: function (body) {
+      var bodyIndex = this.bodies.indexOf(body);
+      if (bodyIndex !== -1) {
+        this.bodies.splice(bodyIndex, 1);
       }
     }
   }
@@ -68,13 +79,83 @@
         this.position.y += 2;
         this.frame = 2;
       }
+      
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
+        var data = this.bulletData();
+        console.log(data);
+        var bullet = new Bullet(
+          this.game,
+          data.position,
+          data.velocity
+        );
+        
+        this.game.addBody(bullet);
+      }
+    },
+    
+    bulletData: function () {
+      var position;
+      var velocity;
+      
+      switch (this.frame) {
+          case 0:
+            position = {x: this.position.x + (this.size.h / 2) - 1, y: this.position.y};
+            velocity = {x: 0, y: -7}
+            break;
+          case 1:
+            position = {x: this.position.x + this.size.h, y: this.position.y + (this.size.w / 2) - 1};
+            velocity = {x: 7, y: 0}
+            break;
+          case 2:
+            position = {x: this.position.x + (this.size.h / 2) - 1, y: this.position.y + this.size.w};
+            velocity = {x: 0, y: 7};
+            break;
+          case 3:
+            position = {x: this.position.x, y: this.position.y + (this.size.w / 2) - 1};
+            velocity = {x: -7, y: 0};
+            break;
+          default:
+            throw('ERROR: unknow number of frame');
+      };
+      
+      return {
+        position: position,
+        velocity: velocity
+      }
     },
     
     draw: function (screen) {
       var img = new Image();
       img.src = 'img/sprite.png';
       
-      screen.drawImage(img, 0 + (this.size.h * this.frame), 18, this.size.w, this.size.h, this.position.x, this.position.y, this.size.w, this.size.h);
+      screen.drawImage(img, 
+         0 + (this.size.h * this.frame), 18, 
+         this.size.w, this.size.h, 
+         this.position.x, this.position.y, 
+         this.size.w, this.size.h
+      );
+    }
+  };
+  
+  var Bullet = function (game, position, velocity) {
+    this.game = game;
+    this.position = position;
+    this.size = {
+      w: 2,
+      h: 3
+    };
+    this.velocity = velocity;
+  };
+  
+  Bullet.prototype = {
+    update: function () {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+    },
+    
+    draw: function (screen) {
+      screen.fillStyle = '#adadad';
+      screen.fillRect(this.position.x, this.position.y, this.size.w, this.size.h);
     }
   };
   
@@ -96,7 +177,8 @@
       LEFT: 37,
       UP: 38,
       RIGHT: 39,
-      DOWN: 40
+      DOWN: 40,
+      SPACE: 32
     },
     
     isDown: function (keyCode) {
