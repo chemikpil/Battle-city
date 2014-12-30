a066ec5f0f282351503534b2f992803dbf67a8de
Maciej Wagner <maciej.wagner@allegro.pl>
2014-12-30 11:57:53 +0100

    WebsocketClient module

--- js/WebsocketClient.js
+++ js/WebsocketClient.js
@@ -1,47 +1,10 @@
 var BattleCity = BattleCity || {}
 
-BattleCity.WebsocketClient = function (game) {
-    this.game = game;
-    this.connection = new WebSocket('ws://localhost:8080');
+BattleCity.WebsocketClient = function (host, callback) {
+    this.connection = new WebSocket(host);
 
-    var self = this;
     this.connection.onmessage = function (e) {
-        //console.log('Receiving: ' + e.data);
-        var data = JSON.parse(e.data);
-        var messageType = data.type;
-        var messageData = data.data;
-
-        if (messageType == 'ID') {
-            var player = new Player(self.game);
-
-            player.id = messageData;
-            player.isHuman = true;
-
-            self.game.bodies[messageData] = player;
-
-            player.notify();
-        }
-
-        else if (messageType == 'player') {
-            var player = new Player(self.game);
-            player.id = messageData.id;
-            player.frame = messageData.frame;
-            player.position.x = messageData.x;
-            player.position.y = messageData.y;
-
-            self.game.bodies[messageData.id] = player;
-        }
-
-        else if (messageType == 'bullet') {
-            self.game.addBody(
-                new Bullet(
-                    self.game,
-                    {'x': messageData.x, 'y': messageData.y},
-                    messageData.velocity,
-                    messageData.id
-                )
-            );
-        }
+        callback(JSON.parse(e.data));
     }
 };
 
