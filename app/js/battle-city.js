6cfd2be82ac5dd5b3ebaf66caae115fc46e1ff65
ChemikPIL <chemikpil@gmail.com>
2015-01-02 00:00:41 +0100

    Focus on name input

--- app/js/battle-city.js
+++ app/js/battle-city.js
@@ -1,6 +1,7 @@
 (function () {
   'use strict';
   var form = document.getElementById('welcome-form');
+  form.name.focus();
   var welcome = document.getElementById('welcome');
   form.addEventListener('submit', function (event) {
     event.preventDefault();
